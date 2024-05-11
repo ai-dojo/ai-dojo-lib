fcbe7eb1d5990492622b5c3a35b09946e0f22c8b
Christian Staudt <mail@clstaudt.me>
2024-05-11 15:57:53 +0200

    requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
 setup(
     name='ai_dojo',
     version=2.0,
@@ -7,6 +12,5 @@ setup(
     author='Christian Staudt',
     url='http://clstaudt.me',
     packages=['ai_dojo'],
-    install_requires=[
-    ]
+    install_requires=requirements,
 )
